79b946e7035744a5dd132e03dde6c9d3a369e308
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-12-04 17:37:41 +0800

    added in c++ code for testing

--- testingjava.java
+++ testingjava.java
@@ -2,7 +2,7 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class ReadFile {
+public class testingjava{
   public static void main(String[] args) {
     try {
       File myObj = new File("filename.csv");
---
+++ writingcc.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    ofstream myFile("testingwriting.csv");
+    
+   
+    myFile << "Foo\n";
+    myFile << "1\n";
+    myFile << "2\n";
+    myFile << "3\n";
+    
+   
+    myFile.close();
+    
+    return 0;
+}
